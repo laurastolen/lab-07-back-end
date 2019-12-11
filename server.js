23d0136153a8c2ecdf958f86b6f4cbc904b323f9
laura antonius <laurastolen@gmail.com>
2019-12-11 13:49:13 -0800

    working on locations route

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 
+// superagent goes and gets API data for us
+const superagent = require('superagent');
+
 // invoking express, setting port, using cors
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,32 +21,47 @@ app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
 // ----------------ROUTES-------------------
 // making location route:
 app.get('/location', (req, res) => {
-  // get the city name they entered
-  let city = req.query.data;
-  // make locationObj which is returned to front end, and which includes the city name, lat/long, and formatted address
-  let locationObj = searchLatToLong(city);
-  res.send(locationObj);
-  console.log(locationObj);
+  try {
+    // get the city name they entered
+    const city = req.query.data;
+    // make locationObj which is returned to front end, and which includes the city name, lat/long, and formatted address
+    searchLatToLong(city, res);
+    // res.send(locationObj);
+    // console.log(locationObj);
+  }
+  catch (error) {
+    console.error(error);
+
+    Response.status(500).send('oh nooooo something went wrong!');
+  }
 });
 
-function searchLatToLong(city) {
+function searchLatToLong(city, response) {
   // get the json data (from file at this point, eventually from API)
-  const geoData = require('./data/geo.json');
-  // find the part we want
-  const geoDataResults = geoData.results[0];
-  // use the selected part to make new object instance
-  const locationObj = new Location(city, geoDataResults);
-  return locationObj;
+  // const geoData = require('./data/geo.json');
+
+  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.GEOCODE_API_KEY}`
+  superagent.get(url)
+    .then(results => {
+      // console.log(results.body.geometry)
+      const locationObj = new Location(city, results.body);
+
+      response.send(locationObj);
+    });
+
+
+  // const geoDataResults = geoData.results[0];
+
 }
 
 // the constructor that takes in the selected part of the served results, and creates a formatted object instance
-function Location(city, geoDataResults) {
+function Location(city, results) {
   // eslint-disable-next-line camelcase
   this.search_query = city;
   // eslint-disable-next-line camelcase
-  this.formatted_query = geoDataResults.formatted_address;
-  this.latitude = geoDataResults.geometry.location.lat;
-  this.longitude = geoDataResults.geometry.location.lng;
+  this.formatted_query = results.body.results[0].formatted_address;
+  this.latitude = results.body.results.geometry.location.lat;
+  this.longitude = results.body.results.geometry.location.lng;
 }
 
 
