e85135a025711de679c3269acb85482a23e8e61e
laura antonius <laurastolen@gmail.com>
2019-12-11 18:13:32 -0800

    location route functioning

--- server.js
+++ server.js
@@ -20,48 +20,61 @@ app.listen(PORT, () => console.log(`listening on port ${PORT}!`));
 
 // ----------------ROUTES-------------------
 // making location route:
-app.get('/location', (req, res) => {
-  try {
-    // get the city name they entered
-    const city = req.query.data;
-    // make locationObj which is returned to front end, and which includes the city name, lat/long, and formatted address
-    searchLatToLong(city, res);
-    // res.send(locationObj);
-    // console.log(locationObj);
-  }
-  catch (error) {
-    console.error(error);
 
-    Response.status(500).send('oh nooooo something went wrong!');
-  }
-});
+app.get('/location', locationHandler);
 
-function searchLatToLong(city, response) {
-  // get the json data (from file at this point, eventually from API)
-  // const geoData = require('./data/geo.json');
-
-  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.GEOCODE_API_KEY}`
+function locationHandler(req, res) {
+  let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${req.query.data}&key=${process.env.GEOCODE_API_KEY}`
   superagent.get(url)
-    .then(results => {
-      // console.log(results.body.geometry)
-      const locationObj = new Location(city, results.body);
-
-      response.send(locationObj);
+    .then(data => {
+      // console.log(data.body.results[0])
+      const geoData = data.body;
+      const locationObj = new Location(req.query.data, geoData);
+      // console.log(locationObj);
+      res.send(locationObj);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
     });
-
-
-  // const geoDataResults = geoData.results[0];
-
 }
+// app.get('/location', (req, res) => {
+//   try {
+//     // get the city name they entered
+//     const city = req.query.data;
+//     // make locationObj which is returned to front end, and which includes the city name, lat/long, and formatted address
+//     let locationObj = searchLatToLong(city);
+//     console.log(locationObj);
+//     res.send(locationObj);
+//   }
+//   catch (error) {
+//     console.error(error);
+
+//     res.status(500).send('oh nooooo something went wrong!');
+//   }
+// });
+
+// function searchLatToLong(city) {
+// const geoData = require('./data/geo.json');
+
+// let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${process.env.GEOCODE_API_KEY}`
+// superagent.get(url)
+//   .then(data => {
+//     // console.log(data.body.results[0])
+//     const geoData = data.body;
+//     const locationObj = new Location(city, geoData);
+//     // console.log(locationObj);
+//     return locationObj;
+//   });
+// }
 
 // the constructor that takes in the selected part of the served results, and creates a formatted object instance
-function Location(city, results) {
+function Location(city, geoData) {
   // eslint-disable-next-line camelcase
   this.search_query = city;
   // eslint-disable-next-line camelcase
-  this.formatted_query = results.body.results[0].formatted_address;
-  this.latitude = results.body.results.geometry.location.lat;
-  this.longitude = results.body.results.geometry.location.lng;
+  this.formatted_query = geoData.results[0].formatted_address;
+  this.latitude = geoData.results[0].geometry.location.lat;
+  this.longitude = geoData.results[0].geometry.location.lng;
 }
 
 
@@ -70,7 +83,6 @@ app.get('/weather', (req, res) => {
   // get the weather data (from local file, eventually API) and format via constructor to return to front end
   const forecastObj = searchWeather();
   res.send(forecastObj);
-  console.log(forecastObj);
 });
 
 // fx to get the data, make instances via constructor, and return formatted info for front end
